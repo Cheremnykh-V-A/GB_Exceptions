ea3e66a232e1160ab456779bed47d9cdc279713e
Vadim Cheremnykh <chere96@yandex.ru>
2023-07-27 09:56:19 +0400

    added Task2

---
+++ src/main/java/Homeworks/Homework2/Task2.java
@@ -0,0 +1,18 @@
+package Homeworks.Homework2;
+
+public class Task2 {
+    /*
+    Если необходимо, исправьте данный код
+     */
+
+//    try {
+//        int[] intArray = new int[9];
+//        int d = 0;
+//        double catchedRes1 = intArray[8] / d;
+//        System.out.println("catchedRes1 = " + catchedRes1);
+//    } catch (ArithmeticException e) {
+//        System.out.println("Catching exception: " + e);
+//    } catch (ArrayIndexOutOfBoundsException e) {
+//        System.out.println("Catching exception: " + e);
+//    }
+}
